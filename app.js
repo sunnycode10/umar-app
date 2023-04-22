62be99aede49d5da3a032ca0ba437fc01601c08c
sunday emmanuel <sunnyemmanuel5@gmail.com>
2023-04-23 00:15:30 +0100

    chang port

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res)=>{
     res.send("<h1>Hello World</h1>")
 })
 
-app.listen(8080, ()=>{
-    console.log("server running on port 8080")
+app.listen(5080, ()=>{
+    console.log("server running on port 5080")
 })
